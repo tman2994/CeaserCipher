aa9676a0aed0e04a27438150669bd6606f6d9538
tristan <Tristan_brown_1@hotmail.co.uk>
2023-06-17 18:32:28 +0100

    update

--- ceaserchiper.py
+++ ceaserchiper.py
@@ -7,31 +7,38 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-if direction != 'encode' or direction != 'decode':
+'''if direction != 'encode' or direction != 'decode':
             print('you did not type cleaarly if you wanted to encode or decode')
             sys.stdout.flush()
-            os.execl(sys.executable, 'python', 'ceaserchiper.py')
+            os.execl(sys.executable, 'python', 'ceaserchiper.py')'''
 
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+shift = shift % 26 
 
 def ceaserchiper( encode_or_decode, plain_text, Shift_amount):
         cipher_text = ''
         print(cipher_text)
         if encode_or_decode == 'encode':
             for letter in plain_text:
-                position = alphabet.index(letter)
-                new_position = position + Shift_amount
-                new_letter = alphabet[new_position]
-                cipher_text += new_letter
-                print(f'this is the cihper: {cipher_text}')
+                if letter in alphabet:
+                    position = alphabet.index(letter)
+                    new_position = position + Shift_amount
+                    new_letter = alphabet[new_position]
+                    cipher_text += new_letter
+                else:
+                    cipher_text += letter
+            print(f'this is the cihper: {cipher_text}')
         elif encode_or_decode== 'decode':
             cipher_text = ''
-            for letter in plain_text: 
-                position = alphabet.index(letter)
-                new_position = position - Shift_amount
-                cipher_text += alphabet[new_position]
-                print(f'this is the cihper: {cipher_text}')
+            for letter in plain_text:
+                if letter in alphabet: 
+                    position = alphabet.index(letter)
+                    new_position = position - Shift_amount
+                    cipher_text += alphabet[new_position]
+                else:
+                    cipher_text += letter
+            print(f'this is the cihper: {cipher_text}')
        
 
         #return 
@@ -39,7 +46,7 @@ def ceaserchiper( encode_or_decode, plain_text, Shift_amount):
 ceaserchiper(encode_or_decode= direction, plain_text=text, Shift_amount= shift)
 
 def restart():
-    restart_cipher = input("press yes to restart cypher: ".lower())
+    restart_cipher = input("press yes to restart cypher, or anyother import to exit: ".lower())
     if restart_cipher == 'yes':
         direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
         text = input("Type your message:\n").lower()
